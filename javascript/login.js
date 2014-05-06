4f96d9ca57f8a830dfede5c89b2054cbdfde6f6d
rbrieuc <remibrieuc@hotmail.com>
2014-05-06 15:44:19 +0200

    modify js to log with enter

--- javascript/login.js
+++ javascript/login.js
@@ -1,26 +1,34 @@
 $(document).ready(function(){
 
- $('#login-button').click(function ()
-	{
-		$.ajax({
-				url : 'login.php',
-				method : 'POST',
-				data : { login : $('#login').val(), password : $('#password').val()},
-        success : function(res)
+ $('#login-button').click(login);
+
+  $(document).keypress(function(e) {
+    if(e.which == 13) {
+        login();
+    }
+  });
+
+function login()
+{
+  $.ajax({
+      url : 'login.php',
+      method : 'POST',
+      data : { login : $('#login').val(), password : $('#password').val()},
+      success : function(res)
+      {
+        res = jQuery.parseJSON(res);
+        console.log(res);
+        if (res.error)
         {
-          res = jQuery.parseJSON(res);
-          console.log(res);
-          if (res.error)
-          {
-            console.log("error");
-            $('.input').addClass("error");
-          }
-          else if (res.auth_token)
-          {
-            console.log("connected");
-            location.reload();
-          }
+          console.log("error");
+          $('.input').addClass("error");
         }
-			});
-	});
+        else if (res.auth_token)
+        {
+          console.log("connected");
+          location.reload();
+        }
+      }
+    });
+}
 });
